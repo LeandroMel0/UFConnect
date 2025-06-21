89890292a9b8f9bcde09dfeeda9366420f124302
LeandroMel0 <lesanto175@gmail.com>
2025-06-21 16:18:41 -0300

    Update user_routes.js

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -4,7 +4,7 @@ import auth from '../middlewares/auth.js';
 import user_controller from '../controllers/user_controller.js';
 import upload from "../config/multer.js";
 
-router.get("/login", user_controller.login); // Login route
+router.post("/login", user_controller.login); // Login route
 router.get('/list', user_controller.get_users);    // list users
 router.post('/create',upload.single("image"), user_controller.create_user);   // Create a new user
 router.delete('/delete/:id', auth.authMiddleware, user_controller.delete_user);  // Delete a user by ID
